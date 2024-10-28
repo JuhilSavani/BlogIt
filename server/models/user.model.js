c6d82e055a756a8446ce0975f2bef6812e6b07cc
JuhilSavani <savanijuhil@gmail.com>
2024-10-28 22:27:54 +0530

    Add data model for a user

---
+++ server/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    index: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: /.+\@.+\..+/,
+    index: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
