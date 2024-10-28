0ceeb47626502c7e6b9c878a910d273544b7e951
JuhilSavani <savanijuhil@gmail.com>
2024-10-28 22:27:30 +0530

    Add data model for a blog

---
+++ server/models/blog.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  tags: {
+    type: [String],
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const Blog = mongoose.model("Blog", blogSchema);
