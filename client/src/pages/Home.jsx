2aaeba230624825ffe9e7bbe6c1613f028796279
JuhilSavani <savanijuhil@gmail.com>
2024-10-29 11:17:23 +0530

    add home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import { Link } from "react-router-dom";
 
-const Home = () => {
+function Home() {
   return (
-    <div className='page home'>
-    <div className="container">Home</div>
-  </div>
+    <div className="page home">
+      <div className="container">
+        <h1>
+          Share&nbsp;your&nbsp;thoughts&nbsp;with&nbsp;<span>BlogIt</span>
+        </h1>
+        <p>
+          Your personal space for microblogging—quickly share moments and ideas!
+        </p>
+        <div>
+          <button>
+            <Link to="/sign-up">Register</Link>
+          </button>
+          <button>
+            <Link to="/sign-in">Login</Link>
+          </button>
+        </div>
+      </div>
+    </div>
   );
-};
+}
 
 export default Home;
