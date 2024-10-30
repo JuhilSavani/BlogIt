682ddabe47280ab61d55176bbd7da3932a9cf3a4
JuhilSavani <savanijuhil@gmail.com>
2024-10-30 10:52:38 +0530

    update configuration of routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,70 @@
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Create from './pages/Create';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+  Outlet,
+  useLocation,
+  matchPath
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Create from "./pages/Create";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import ProtectedLoader from "./utils/ProtectedLoader";
+import Blog from "./components/Blog";
 
-// Define a layout component 
+// Define a layout component
 const Layout = () => {
+  const location = useLocation();
+  const navbarRoutes = [
+    "/",
+    "/about",
+    "/sign-in",
+    "/sign-up",
+    "/create"
+  ];
+  const isDashboardRoute = matchPath("/dashboard/:username", location.pathname);
+
+  const showNavbar = navbarRoutes.includes(location.pathname) || isDashboardRoute;
   return (
     <>
-      <Navbar />
-      <Outlet /> 
+      {showNavbar && <Navbar />}
+      <Outlet />
     </>
   );
 };
 
-
 // Define the routes
 const routes = createRoutesFromElements(
   <Route path="/" element={<Layout />}>
-    <Route index element={<Home />} />
-    <Route path="about" element={<About />} />
-    <Route path="create" element={<Create />} />
+    <Route element={<ProtectedLoader />}>
+      {/* Public */}
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="sign-in" element={<Login />} />
+      <Route path="sign-up" element={<Register />} />
+
+      {/* Protected */}
+      <Route element={<ProtectedRoute />}>
+        <Route path="create" element={<Create />} />
+        <Route path="dashboard/:username" element={<Dashboard />} />
+        <Route path="blog/:id" element={<Blog />} />
+      </Route>
+
+      {/* Catch all */}
+      <Route path="*" element={<NotFound />} />
+    </Route>
   </Route>
 );
 
-
 const App = () => {
-  return (
-    <RouterProvider router={createBrowserRouter(routes)} />
-  );
+  return <RouterProvider router={createBrowserRouter(routes)} />;
 };
 
 export default App;
