f230b63f1aacc014630ad8ac235ac22c61bafc37
JuhilSavani <savanijuhil@gmail.com>
2024-10-31 07:07:47 +0530

    complete submission, notification

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,71 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../utils/hooks/useAuth";
+import axios from "../utils/apis/axios";
+import useNotify from "../utils/hooks/useNotify";
+import useValidator from "../utils/hooks/useValidator";
+
+
 const Login = () => {
+  const { setAuth } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState({});
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location?.state?.from?.pathname || "/";
+
+  const notify = useNotify();
+  const validate = useValidator();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+    const validationError = validate(data, {
+      type: "login"
+    });
+    if (validationError) {
+      setError(validationError);
+      setIsLoading(false);
+      return; 
+    }
+
+    try {
+      const response = await axios.post("/auth/login", data, {
+        withCredentials: true, // Allows the server to set httpOnly cookie
+      });
+      setAuth(response.data);
+      // Login successful! Redirecting you now...
+      notify("success", "Great to see you again!");
+      navigate(from, { replace: true });
+    } catch (err) {
+      setError(err?.response?.data ? err.response.data.message : err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(()=>{
+    if(error){
+      notify("error", error);
+      setError("");
+    }
+  }, [error, notify]);
+
   return (
     <div className="page sign-in">
       <div className="container">
         <h1>Welcome Back</h1>
         <span>Enter your credentials correctly to log in.</span>
-        <form>
+        <form onSubmit={handleSubmit} noValidate>
           <label htmlFor="username">Username: </label>
           <input
             type="text"
             id="username"
             name="username"
-            placeholder="username"
+            placeholder="Username"
             required
           />
           <label htmlFor="password">Password: </label>
@@ -20,13 +73,13 @@ const Login = () => {
             type="password"
             id="password"
             name="password"
-            placeholder="password"
+            placeholder="Password"
             required
           />
           <span>
             Not register yet? <Link to="/sign-up">Sign up</Link>
           </span>
-          <button type="submit">Login</button>
+          <button type="submit">{isLoading ? "Loading" : "Login"}</button>
         </form>
       </div>
     </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,19 +1,69 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../utils/hooks/useAuth";
+import axios from "../utils/apis/axios";
+import useValidator from "../utils/hooks/useValidator";
+import useNotify from "../utils/hooks/useNotify";
 
 const Register = () => {
+  const { setAuth } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState({});
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location?.state?.from?.pathname || "/";
+
+  const notify = useNotify();
+  const validate = useValidator();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+    // Validate the data
+    const validationError = validate(data, {
+      type: "register"
+    });
+    if (validationError) {
+      setError(validationError);
+      setIsLoading(false);
+      return; 
+    }
+    try {
+      const response = await axios.post("/auth/register", data, {
+        withCredentials: true
+      });
+      setAuth(response.data);
+      notify("success", "Your account has been created.");
+      navigate(from, { replace: true });
+    } catch (err) {
+      setError(err?.response?.data ? err.response.data.message : err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(()=>{
+    if(error){
+      notify("error", error);
+      setError("");
+    }
+  }, [error, notify]);
+
   return (
     <div className="page sign-up">
       <div className="container">
         <h1>Hello, There!</h1>
         <span>Fill out this form to create your account.</span>
-        <form>
+        <form onSubmit={handleSubmit} noValidate>
           <label htmlFor="username">Username: </label>
           <input
             type="text"
             id="username"
             name="username"
-            placeholder="username"
+            placeholder="Username"
             required
           />
           <label htmlFor="email">Email: </label>
@@ -21,7 +71,7 @@ const Register = () => {
             type="email"
             id="email"
             name="email"
-            placeholder="email"
+            placeholder="Email"
             required
           />
           <label htmlFor="password">Password: </label>
@@ -29,13 +79,13 @@ const Register = () => {
             type="password"
             id="password"
             name="password"
-            placeholder="password"
+            placeholder="Password"
             required
           />
           <span>
             Already have an account? <Link to="/sign-in">Sign in</Link>
           </span>
-          <button type="submit">Register</button>
+          <button type="submit">{isLoading ? "Loading" : "Register"}</button>
         </form>
       </div>
     </div>
