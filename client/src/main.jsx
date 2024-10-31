5801a2eb68bde38a90b98fb885f41537cb17be87
JuhilSavani <savanijuhil@gmail.com>
2024-10-31 11:41:21 +0530

    remove strictmode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./main.scss";
@@ -6,11 +6,11 @@ import { AuthProvider } from "./utils/contexts/AuthProvider.jsx";
 import { ThemeProvider } from "./utils/contexts/ThemeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  // <StrictMode>
     <ThemeProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
     </ThemeProvider>
-  </StrictMode>
+  // </StrictMode>
 );
