f6059024a26eee6b3a20c74fed74d1d1f2bbbcbb
JuhilSavani <savanijuhil@gmail.com>
2024-11-22 14:25:17 +0530

    refactor: authorization api path names

--- client/src/utils/hooks/useLogout.jsx
+++ client/src/utils/hooks/useLogout.jsx
@@ -5,7 +5,7 @@ function useLogout() {
   const { setAuth } = useAuth();
   return async () => {
     try {
-      await axios.post("/auth/logout", null, { withCredentials: true });
+      await axios.post("/authorize/logout", null, { withCredentials: true });
       setAuth({});
     } catch (err) {
       console.log("Error: ", err.message);
--- client/src/utils/hooks/useRefresh.jsx
+++ client/src/utils/hooks/useRefresh.jsx
@@ -3,7 +3,7 @@ import axios from "../apis/axios";
 const useRefresh = () => {
   return async () => {
     try {
-      const response = await axios.get("/auth/refresh", {
+      const response = await axios.get("/authorize/refresh", {
         withCredentials: true,
       });
       return response;
